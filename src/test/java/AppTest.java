e8a426d25ef32fae863fbde3c3f9d2834c313b69
Polina Nenchev <polina.mihova@yahoo.com>
2016-02-15 16:16:54 -0800

    Use template for java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Task list");
+  }
+
+  @Test
+  public void checkIfTaskDisplayed() {
+      goTo("http://localhost:4567/");
+      fill("#description").with("Wash the dishes");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      assertThat(pageSource()).contains("Wash the dishes");
+  }
